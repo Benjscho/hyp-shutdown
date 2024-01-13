15bfd2db4daec973199042fdd2abf18ac65bbb97
Ben Schofield <me@benjscho.dev>
2024-01-12 17:17:37 -0800

    Test graceful shutdown of hyper server

---
+++ src/bin/client.rs
@@ -0,0 +1,22 @@
+use tokio::net::TcpStream;
+
+
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Parse our URL...
+    let url = "http://127.0.0.1:3000".parse::<hyper::Uri>()?;
+
+    // Get the host and the port
+    let host = url.host().expect("uri has no host");
+    let port = url.port_u16().unwrap_or(80);
+
+    let address = format!("{}:{}", host, port);
+
+    // Open a TCP connection to the remote host
+    let stream = TcpStream::connect(address).await?;
+
+    std::future::pending::<()>().await;
+
+    Ok(())
+}
